dd94a7ec0873dc99449ab8d4437dfa2414f8c6f2
Jon Wenger <jonwenger783@gmail.com>
2018-05-02 15:41:57 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,18 +1,18 @@
 def fizzbuzz(int)
 
-if int % 5 == 0 && int % 3 == 0
-     "FizzBuzz"  
+if int % 5 == 0 & int % 3 == 0
+     "FizzBuzz"
 
    elsif int % 3 == 0
     "Fizz"
-  
+
 elsif int % 5 == 0
   "Buzz"
 
 
 
-  
-    
+
+
   end
 
 
